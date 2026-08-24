4ae26bf61ed83ebb8f4611218b6d6c5bc1fe0a99
cadimodev <cadimodev@gmail.com>
2026-08-24 11:44:23 +0200

    chore: initialize multi-module Spring Boot project

---
+++ kodo-api/src/main/java/com/kodo/api/KodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kodo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodoApiApplication.class, args);
+	}
+
+}
---
+++ kodo-worker/src/main/java/com/kodo/worker/KodoWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.kodo.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodoWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodoWorkerApplication.class, args);
+	}
+
+}
