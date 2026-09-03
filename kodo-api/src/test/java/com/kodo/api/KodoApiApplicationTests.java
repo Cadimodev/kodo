ab378fb4490037724aa78a7144ed5e53ec0f1594
cadimodev <cadimodev@gmail.com>
2026-09-03 14:47:31 +0200

    test(api): use PostgreSQL Testcontainer for context test

--- kodo-api/src/test/java/com/kodo/api/KodoApiApplicationTests.java
+++ kodo-api/src/test/java/com/kodo/api/KodoApiApplicationTests.java
@@ -2,12 +2,39 @@ package com.kodo.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class KodoApiApplicationTests {
+
+	@Container
+	static final PostgreSQLContainer postgres =
+			new PostgreSQLContainer("postgres:17");
+
+	@DynamicPropertySource
+	static void configurePostgres(
+			DynamicPropertyRegistry registry
+	) {
+		registry.add(
+				"spring.datasource.url",
+				postgres::getJdbcUrl
+		);
+		registry.add(
+				"spring.datasource.username",
+				postgres::getUsername
+		);
+		registry.add(
+				"spring.datasource.password",
+				postgres::getPassword
+		);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
